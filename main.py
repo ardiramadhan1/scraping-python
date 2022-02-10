58f57bd5922b92daddcd229520a73c7ef576b83e
ardiramadhan1 <80233611+ardiramadhan1@users.noreply.github.com>
2022-02-10 20:13:23 +0700

    update content scraping

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ import time
 
 app = Flask(__name__)
 PATH = r"C:\Users\user\Downloads\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
 
 
 @app.route('/bandung')
 def bandung():
+    driver = webdriver.Chrome(PATH)
     driver.get("https://www.liputan6.com")
     search = driver.find_element(By.NAME, "q")
     search.send_keys("Bandung")
@@ -30,21 +30,59 @@ def bandung():
             image = article.find_element(By.CSS_SELECTOR, 'img').get_attribute('src')
             try:
                 category = \
-                    article.find_element(By.CSS_SELECTOR, 'a .articles--iridescent-list--text-item__category').text
+                    article.find_element(By.CLASS_NAME, 'articles--iridescent-list--text-item__category').text
             except Exception as e:
                 category = None
+            try:
+                waktu = \
+                    article.find_element(By.CLASS_NAME, 'articles--iridescent-list--text-item__datetime').text
+            except Exception as e:
+                waktu = None
+            try:
+                title = \
+                    article.find_element(By.CLASS_NAME, 'articles--iridescent-list--text-item__title-link-text').text
+            except Exception as e:
+                title = None
 
             rows.append({
                 'url': url,
+                'title': title,
                 'image': image,
-                'category': category
+                'category': category,
+                'time': waktu
             })
         driver.quit()
+        return jsonify({
+            'data': rows
+        })
     except Exception as e:
         return e
-    return jsonify({
-        'data': rows
-    })
+
+
+@app.route('/content')
+def bandung_content():
+    url = "https://www.liputan6.com/regional/read/4883330/tamu-hotel-di-bandung-meninggal-terperosok-dari-lantai-3"
+    driver = webdriver.Chrome(PATH)
+    driver.get(url)
+    try:
+        konten = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'read-page-upper'))
+        )
+        title = konten.find_element(By.CLASS_NAME, 'read-page--header--title').text
+        author = konten.find_element(By.CLASS_NAME, 'read-page--header--author__name').text
+        content = konten.find_element(By.CLASS_NAME, 'article-content-body__item-content').text
+        date = konten.find_element(By.CLASS_NAME, 'read-page--header--author__datetime').text
+        driver.quit()
+        return jsonify({
+            'data': {
+                "title": title,
+                "author": author,
+                "content": content,
+                "date": date
+            }
+        })
+    except Exception as e:
+        return e
 
 
 if __name__ == '__main__':
